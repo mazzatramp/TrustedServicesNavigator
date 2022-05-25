bff4bf433b03ff09bdd33ad89c5c53798f4e212b
Mazzarotto Filippo <filippomazzarotto@gmail.com>
2022-05-25 19:15:19 +0200

    Simplify ProviderFilter
    - Rename CountryProviderFilter to ProviderFilter
    - Remove filter by country function

--- src/main/java/com/tsn/trustedservicesnavigator/ProviderFilter.java
+++ src/main/java/com/tsn/trustedservicesnavigator/ProviderFilter.java
@@ -1,31 +1,10 @@
 package com.tsn.trustedservicesnavigator;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class ProviderFilter extends Filter {
-
-    private List<String> whitelist;
-
-    public ProviderFilter() {
-        this.whitelist = new ArrayList<>();
-    }
-
     @Override
-    public void applyTo(TrustedList listToFilter) {
-        if (!whitelist.isEmpty()) {
-            filterByWhitelist(listToFilter);
-        }
-    }
-
-    @Override
-    public void setWhitelist(List<String> whitelist) {
-        this.whitelist = whitelist;
-    }
-
-    private void filterByWhitelist(TrustedList listToFilter) {
+    protected void filterByWhitelist(TrustedList listToFilter) {
         listToFilter.getCountries().forEach(country -> {
             country.getProviders().removeIf(provider -> !whitelist.contains(provider.getName()));
         });
