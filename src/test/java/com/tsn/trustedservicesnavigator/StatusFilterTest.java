59c22ac12036d74592dc1c721d27f9705bea4b4f
Riccardo1000 <riccardocontelav@gmail.com>
2022-05-25 21:41:54 +0200

    Creation StatusFilterTest

---
+++ src/test/java/com/tsn/trustedservicesnavigator/StatusFilterTest.java
@@ -0,0 +1,31 @@
+package com.tsn.trustedservicesnavigator;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatusFilterTest {
+    /*private Service getService1(){
+     Service servizio1 = null;
+     servizio1.setStatus("http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/withdrawn");
+     servizio1.serviceName
+
+    }*/
+
+   /* @Test
+    void Service1_equals_Service1_ReturnsTrue()
+    {
+        //arrange
+
+
+        //act
+        String actualName = ITA.getName();
+
+        //assert
+        assertEquals("Italy",actualName);
+    }*/
+
+
+}
