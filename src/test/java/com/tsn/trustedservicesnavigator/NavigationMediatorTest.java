a52516690d41db9931c70acca589f78dcc33fd70
Riccardo1000 <riccardocontelav@gmail.com>
2022-05-26 19:10:00 +0200

    Created NavigationMediatorTest

---
+++ src/test/java/com/tsn/trustedservicesnavigator/NavigationMediatorTest.java
@@ -0,0 +1,13 @@
+package com.tsn.trustedservicesnavigator;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NavigationMediatorTest {
+    private UserInterfaceController userInterfaceController;
+        }
+
+
+
