78cc3280cb713b5118aabe49b22c16f9b16d863d
Riccardo1000 <riccardocontelav@gmail.com>
2022-05-26 12:59:35 +0200

    Create Help
    Class in Test folder with helper methods

---
+++ src/test/java/com/tsn/trustedservicesnavigator/Help.java
@@ -0,0 +1,15 @@
+package com.tsn.trustedservicesnavigator;
+
+import java.io.IOException;
+
+public class Help {
+    private static TrustedList wholeList;
+    private static void constructWholeList() throws IOException {
+        wholeList= new TrustedList();
+        wholeList.downloadApiData();
+    }
+    public static TrustedList getWholeList() throws IOException {
+        if(wholeList==null) {constructWholeList();}
+        return wholeList;
+    }
+}
