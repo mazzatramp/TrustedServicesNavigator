4fd8a9bba670c44dbaec7c62e3e788c346d608ff
Riccardo1000 <riccardocontelav@gmail.com>
2022-05-31 23:56:59 +0200

    Create FilterSelectionAccordionTest

---
+++ src/test/java/com/trustedservices/navigator/components/FilterSelectionAccordionTest.java
@@ -0,0 +1,9 @@
+package com.trustedservices.navigator.components;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterSelectionAccordionTest {
+    //COSTRUTTORE
+    //FORSE POSSO TESTARE MOLTI METODI MA DEVO CAPIRE SE POSSO USARE IL GET
+
+}
