9b9a7457e2ad66c4868e0151c0641ccf0e0fd84a
Mazzarotto Filippo <filippomazzarotto@gmail.com>
2022-05-31 13:29:49 +0200

    Delete merge residual files

--- src/main/java/com/tsn/trustedservicesnavigator/frontend/filters/Filter.java
+++
@@ -1,8 +0,0 @@
-package com.tsn.trustedservicesnavigator.frontend.filters;
-
-import com.tsn.trustedservicesnavigator.backend.TrustedList;
-
-public abstract class Filter {
-    public abstract void applyTo(TrustedList listToFilter);
-}
-
