a855fae70c175ab08386cabe4d4333c37a6d1de4
Mazzarotto Filippo <filippomazzarotto@gmail.com>
2022-06-03 00:49:40 +0200

    Cleaned NavigationControllerTest.java
    Remove comment noise

--- src/test/java/com/trustedservices/NavigationControllerTest.java
+++ src/test/java/com/trustedservices/NavigationControllerTest.java
@@ -1,83 +1,26 @@
 package com.trustedservices;
 import com.trustedservices.navigator.NavigationController;
-import com.trustedservices.navigator.WindowController;
 import org.junit.jupiter.api.*;
 
 @DisplayName("A NavigationController")
 class NavigationControllerTest {
-    //QUESTO HA SOLO METODI SEMPLICI
-
-
-
-    @Nested
-    @DisplayName("when null")
-    class WhenNull {
-        @BeforeEach
-        void setOggettoNull() {
-            //nomeOggetto=null;
-        }
-        @DisplayName("and I use the method ...")
-        @Nested
-        class nomeMetodo1{}
-        @DisplayName("and I use the method ...")
-        @Nested
-        class nomeMetodo2{}
-    }
-
-    @Test
-    @DisplayName("is instantiated with new NavigationController()")
-    void isInstantiatedWithNewClasse() {
-        new NavigationController();
-    }
 
     NavigationController navigationController;
 
+    @Test
+    @DisplayName("is instantiated with new NavigationController()")
+    void isInstantiatedWithNewClass() {
+        new NavigationController();
+    }
+
     @Nested
     @DisplayName("when new")
-    class WhenNew{
+    class WhenNew {
 
         @BeforeEach
         @Test
         void createANavigationMediator() {
             navigationController = new NavigationController();
         }
-
-        @Disabled
-        @DisplayName("and I use the method readActiveFiltersFrom")
-        @Test
-        void prova(){
-                WindowController windowController = new WindowController();
-                //assertEquals(navigationController.getFilteredList().getCountries().getProviders, windowController.getFilterAccordion().getSelectedProviders());
-            }
-        }
-
-        @DisplayName("and I use the method ..")
-        @Nested
-        class nomeMetodo2{}}
-
-/*
-    @Test
-    void setUserInterfaceController() {
-    }
-
-    @Test
-    void getFilteredList() {
-    }
-
-    @Test
-    void getCompleteList() {
-    }
-
-    @Test
-    void fillCompleteListFromApiData() {
-    }
-
-    @Test
-    void getFilterController() {
-    }
-
-    @Test
-    void readActiveFiltersFrom() {
     }
 }
-*/
