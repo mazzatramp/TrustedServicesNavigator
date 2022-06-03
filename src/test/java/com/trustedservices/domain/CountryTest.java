f30b3d3a6b3c6bd8c99f7cd9db1a206e7d4404c7
Riccardo1000 <riccardocontelav@gmail.com>
2022-06-04 00:35:54 +0200

    Refactors CountryTest and ProviderTest

--- src/test/java/com/trustedservices/domain/CountryTest.java
+++ src/test/java/com/trustedservices/domain/CountryTest.java
@@ -14,69 +14,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class CountryTest {
     Country country;
 
-    @Nested
-    @DisplayName("when null")
-    class WhenNull {
-        @BeforeEach
-        void setCountryNull() {
-            country = null;
-        }
-
-        @Nested
-        @DisplayName("and I use the method clone()")
-        class Clone {
-            @DisplayName("returns an Error")
-            @Test
-            void clone_NullCountry_returnsError() {
-                assertThrows(NullPointerException.class, () -> country.clone());
-            }
-        }
-
-        @DisplayName("and I use the method equals(Object)")
-        @Nested
-        class Equals{
-            Country argumentCountry;
-            @DisplayName("with a country as argument, it returns an Error")
-            @Test
-            void clone_NullCountry_returnsError() {
-                argumentCountry = new Country("Austria", "AT");
-                //SAREBBE DA METTERE UN ERRORE PIU SPECIFICO NEL
-                //arrange
-                //assert
-                assertThrows(NullPointerException.class, () -> country.equals(argumentCountry));
-                //assertEquals(getWholeList(),clonedList)
-            }
-            @DisplayName("with a null country as argument, it returns error ")
-            @Test
-            void prova() {
-                argumentCountry = null;
-                //SAREBBE DA METTERE UN ERRORE PIU SPECIFICO NEL
-                //arrange
-                //assert
-                assertThrows(NullPointerException.class, () -> country.equals(argumentCountry));
-                //assertEquals(getWholeList(),clonedList)
-            }
-
-
-        }
-        @Nested
-        class CompareTo{
-           Country countryAsArgument;
-           @DisplayName("with a country returns an Error")
-           @Test
-           void clone_NullCountry_returnsError() {
-               countryAsArgument = new Country("Austria", "AT");
-               assertThrows(NullPointerException.class, () -> country.compareTo(countryAsArgument));
-           }
-           @DisplayName("with null returns error ")
-           @Test
-           void prova() {
-               countryAsArgument = null;
-               assertThrows(NullPointerException.class, () -> country.compareTo(countryAsArgument));
-           }
-        }
-    }
-
     @Test
     @DisplayName("is instantiated with new Country(String,String)")
     void isInstantiatedWithNewCountry() {
@@ -88,50 +25,56 @@ class CountryTest {
     class WhenNew {
         @BeforeEach
         void createACountry() {
-            country = new Country("Austria", "AT");
+            country = new Country("Austria", "AT"); //SE USO COSI' IL COSTRUTTURE PERO' DOPO IL METODO CLONE NON LO TESTA TUTTO
         }
 
         @DisplayName("and I use the method equals(Object)")
         @Nested
         class Equals {
-            @DisplayName("with a country as argument")
-            @Nested
-            class CountryAsArgument {
-                Country argumentCountry;
-
-                @DisplayName("and the two countries are the same, method should return true")
+            Country argumentCountry;
+                @DisplayName("and the two countries are the same object, method should return true")
                 @Test
-                void SameCountryAsArgument() {
+                void SameCountryObjectAsArgument() {
                     //arrange
                     argumentCountry = country;
                     //act
                     boolean areCountriesTHeSame = country.equals(argumentCountry);
                     //assert
                     assertTrue(areCountriesTHeSame);
+
                 }
 
-                @DisplayName("and the two countries are not the same, method should return false")
+            @DisplayName("and the two countries are the same , method should return true")
+            @Test
+            void SameCountryAsArgument() {
+
+                argumentCountry =  new Country("Austria", "AT");;
+
+                boolean areCountriesTHeSame = country.equals(argumentCountry);
+
+                assertTrue(areCountriesTHeSame);
+            }
+
+            @DisplayName("and the two countries are not the same, method should return false")
                 @Test
                 void NotSameCountryAsArgument() {
-                    //arrange
-                     argumentCountry = new Country("Italia", "IT");
-                    //act
+
+                    argumentCountry = new Country("Italia", "IT");
+
                     boolean areCountriesTHeSame = country.equals(argumentCountry);
-                    //assert
+
                     assertFalse(areCountriesTHeSame);
                 }
                 @DisplayName("with a country null as argument, method should return false")
                 @Test
                 void NullAsArgument() {
-                    //arrange
-                     argumentCountry = null;
-                    //act
-                    boolean areCountriesTHeSame = country.equals(argumentCountry);
-                    //assert
-                    assertFalse(areCountriesTHeSame);
-                }
-            }
 
+                    argumentCountry = null;
+
+                    boolean areCountriesTheSame = country.equals(argumentCountry);
+
+                    assertFalse(areCountriesTheSame);
+                }
             /*
             DOVREI FARLO CON STESSI VALORI MA OGGETTO DIVERSO, NON PENSO NEL CASO PRIMA FOSSE COSI
             */
@@ -140,33 +83,29 @@ class CountryTest {
         @DisplayName("and I use the method compareTo(Country)")
         @Nested
         class CompareTo {
-            @DisplayName("with a country as argument")
-            @Nested
-            class CountryAsArgument {
-                Country argumentCountry;
+
+               Country argumentCountry;
 
                 @DisplayName("and the two countries are the same, method should return 0")
                 @Test
                 void SameCountryAsArgument() throws IOException {
-                    //arrange
+
                     argumentCountry = country;
-                    //act
+
                     int comparison = country.compareTo(argumentCountry);
                     int expectedreturn = 0;
-                    //assert
+
                     assertEquals(expectedreturn, comparison);
                 }
 
-                //COUNTRY MAGGIORE
-                //DEVO METTERE ANCHE DIFFERENZA O BASTA POS E NEG?
                 @DisplayName("and the argument country is greater, method should return a negative number")
                 @Test
                 void aCountry_CompareTo_BiggerCountry_ReturnNegative() throws IOException {
-                    //arrange
+
                     argumentCountry = Help.getCountryN(1);
-                    //act
+
                     int comparison = country.compareTo(argumentCountry);
-                    //assert
+
                     assertTrue(comparison < 0);
 
                 }
@@ -175,17 +114,20 @@ class CountryTest {
                 void aCountry_CompareTo_LowerCountry_ReturnNegative() {
                     argumentCountry = country;
                     country = Help.getCountryN(1);
+
                     int comparison = country.compareTo(argumentCountry);
+
                     assertTrue(comparison > 0);
                 }
                 @DisplayName("and the argument country is null, method should return a error")
                 @Test
                 void aCountry_CompareTo_null() {
                     Country argumentCountry = null;
+
                     assertThrows(NullPointerException.class, () -> country.compareTo(argumentCountry));
                 }
             }
-        }
+
 
         @DisplayName("and I use the method clone()")
         @Nested
@@ -194,10 +136,11 @@ class CountryTest {
             @Test
             void cloneACountryReturnsSameCountry() {
                 Country countryToBeCloned = country;
+
                 Country clonedCountry = countryToBeCloned.clone();
+
                 assertEquals(countryToBeCloned, clonedCountry);
             }
-
         }
     }
 }
--- src/test/java/com/trustedservices/domain/ProviderTest.java
+++ src/test/java/com/trustedservices/domain/ProviderTest.java
@@ -1,12 +1,8 @@
 package com.trustedservices.domain;
 
-import com.sun.source.tree.Tree;
-import com.trustedservices.Help;
 import org.junit.jupiter.api.*;
-
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +12,7 @@ class ProviderTest {
 Provider provider;
 
     @Test
-    @DisplayName("is instantiated with new Provider(String,Int, String,String,List,List)")
+    @DisplayName("is instantiated with new Provider(String,Int, String,String,List<ServiceType>,List<providerServices>)")
     void isInstantiatedWithNewProvider() {
         TreeSet<String> providerServiceTypes = new TreeSet<>(
                 List.of("QCertESeal", "QCertESig", "QTimestamp")
@@ -33,46 +29,69 @@ Provider provider;
     @Nested
     @DisplayName("when new")
     class WhenNew{
+        @BeforeEach
+        void createProvider() {
+            TreeSet<String> providerServiceTypes = new TreeSet<>(
+                    List.of("QCertESeal", "QCertESig", "QTimestamp")
+            );
+
+            TreeSet<Service> providerServices = new TreeSet<>();
+
+            provider = new Provider(0, "TestProvider", "TTT-000-X01", providerServiceTypes, providerServices);
+
+            providerServices.add(new Service(1, "Service 1", "QC", "granted", providerServiceTypes));
+            providerServices.forEach(service -> service.setProvider(provider));
+        }
 
         @DisplayName("and I use the method equals(Object)")
         @Nested
         class Equals {
-            @DisplayName("with a provider as argument")
-            @Nested
-            class CountryAsArgument {
-                Provider argumentProvider;
+
+            Provider argumentProvider;
 
                 @DisplayName("and the two providers are the same, method should return true")
                 @Test
-                void SameCountryAsArgument() {
+                void SameProviderAsArgument() {
                     TreeSet<String> providerServiceTypes = new TreeSet<>(
                             List.of("QCertESeal", "QCertESig", "QTimestamp")
                     );
 
                     TreeSet<Service> providerServices = new TreeSet<>();
-                    provider = new Provider(0, "TestProvider", "TTT-000-X01", providerServiceTypes, providerServices);
-                    argumentProvider = new Provider(0, "TestProvider", "TTT-000-X01",
-                            (TreeSet) providerServiceTypes.clone(), (TreeSet)providerServices.clone());
+
+                    argumentProvider = new Provider(0, "TestProvider", "TTT-000-X01", providerServiceTypes, providerServices);
+
+                    providerServices.add(new Service(1, "Service 1", "QC", "granted", providerServiceTypes));
+                    providerServices.forEach(service -> service.setProvider(provider));
 
                     assertEquals(provider, argumentProvider);
                 }
 
                 @DisplayName("and the two providers are not the same, method should return false")
                 @Test
-                void NotSameCountryAsArgument() {
+                void NotSameProviderAsArgument() {
                     TreeSet<String> providerServiceTypes = new TreeSet<>(
                             List.of("QCertESeal", "QCertESig", "QTimestamp")
                     );
 
                     TreeSet<Service> providerServices = new TreeSet<>();
-                    provider = new Provider(0, "TestProvider", "TTT-000-X01", providerServiceTypes, providerServices);
-                    argumentProvider = new Provider(1, "TestProvider2", "TTT-000-X02",
-                            (TreeSet) providerServiceTypes.clone(), (TreeSet)providerServices.clone());
+
+                    argumentProvider = new Provider(0, "TestProvider2", "TTT-000-X02", providerServiceTypes, providerServices);
+
+                    providerServices.add(new Service(1, "Service 1", "QC", "granted", providerServiceTypes));
+                    providerServices.forEach(service -> service.setProvider(provider));
 
                     assertNotEquals(provider, argumentProvider);
                 }
-            }
+            @DisplayName("with a provider null as argument, method should return false")
+            @Test
+            void NullAsArgument() {
 
+                argumentProvider = null;
+
+                boolean areProvidersTheSame = provider.equals(argumentProvider);
+
+                assertFalse(areProvidersTheSame);
+            }
         /*
         DOVREI FARLO CON STESSI VALORI MA OGGETTO DIVERSO, NON PENSO NEL CASO PRIMA FOSSE COSI
         */
@@ -81,64 +100,71 @@ Provider provider;
         @DisplayName("and I use the method compareTo(Provider)")
         @Nested
         class CompareTo {
-            @DisplayName("with a provider as argument")
-            @Nested
-            class CountryAsArgument {
+
                 Provider argumentProvider;
 
                 @DisplayName("and the two providers are the same, method should return 0")
                 @Test
 
-                void SameCountryAsArgument() throws IOException {
+                void SameProviderAsArgument() {
 
-                    //arrange
                     argumentProvider = provider;
-                    //act
 
                     int comparison = provider.compareTo(argumentProvider);
-                    int expectedreturn = 0;
+                    int expectedReturn = 0;
                     //assert
-                    assertEquals(expectedreturn, comparison);
+                    assertEquals(expectedReturn, comparison);
 
                 }
 
-                //COUNTRY MAGGIORE
-                //DEVO METTERE ANCHE DIFFERENZA O BASTA POS E NEG?
                 @DisplayName("and the argument provider is greater, method should return a negative number")
                 @Test
-                void aCountry_CompareTo_BiggerCountry_ReturnNegative() {
-                    //arrange
-                    //METTERNE UNO CON PIU'SENSO
-                    argumentProvider = Help.getCountryN(1).getProviders().stream().findAny().get();
-                    //act
+                void aProviderCompareToBiggerProviderReturnNegative() {
+
+                    TreeSet<String> providerServiceTypes = new TreeSet<>(
+                            List.of("QCertESeal", "QCertESig", "QTimestamp")
+                    );
+
+                    TreeSet<Service> providerServices = new TreeSet<>();
+
+                    argumentProvider = new Provider(0, "TestProvider2", "TTT-000-X02", providerServiceTypes, providerServices);
+
+                    providerServices.add(new Service(1, "Service 1", "QC", "granted", providerServiceTypes));
+                    providerServices.forEach(service -> service.setProvider(provider));
+
                     int comparison = provider.compareTo(argumentProvider);
-                    //assert
+
                     assertTrue(comparison < 0);
 
                 }
                 @DisplayName("and the argument provider is lower, method should return a positive number")
                 @Test
-                void aCountry_CompareTo_LowerCountry_ReturnNegative() {
-                    //arrange
-                    argumentProvider =provider;
-                    provider = Help.getCountryN(1).getProviders().stream().findAny().get();
-                    //act
+                void aProviderCompareToLowerProviderReturnPositive() {
+
+                    TreeSet<String> providerServiceTypes = new TreeSet<>(
+                            List.of("QCertESeal", "QCertESig", "QTimestamp")
+                    );
+
+                    TreeSet<Service> providerServices = new TreeSet<>();
+
+                    argumentProvider = new Provider(0, "TestProvider", "TTT-000-X00", providerServiceTypes, providerServices);
+
+                    providerServices.add(new Service(1, "Service 1", "QC", "granted", providerServiceTypes));
+                    providerServices.forEach(service -> service.setProvider(provider));
+
                     int comparison = provider.compareTo(argumentProvider);
-                    //assert
+
                     assertTrue(comparison > 0);
 
                 }
-                @DisplayName("and the argument provider is null, method should return a error")
+                @DisplayName("and the argument provider is null, method should return NullPointerException")
                 @Test
-                void aCountry_CompareTo_null() {
-                    //arrange
+                void aProviderCompareToNullReturnException() {
                     Provider argumentProvider = null;
-                    //act
+
                     assertThrows(NullPointerException.class, () -> provider.compareTo(argumentProvider));
-
-
                 }
-            }
+
 
             //COUNTRY MINORE
             //NULL A SX FATTO
@@ -157,9 +183,9 @@ Provider provider;
         class Clone {
             @DisplayName("It returns the same provider")
             @Test
-            void cloneACountryReturnsSameProvider() throws IOException {
+            void cloneAProviderReturnsSameProvider() throws IOException {
                 //arrange
-                Provider providerToBeCloned = Help.getCountryN(0).getProviders().stream().findAny().get();
+                Provider providerToBeCloned = provider;
                 //act
                 Provider clonedProvider = providerToBeCloned.clone();
                 //assert
