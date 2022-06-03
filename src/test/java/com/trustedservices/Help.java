e6980a35b61cc1a16bb1dc56474348e9767779ca
Mazzarotto Filippo <filippomazzarotto@gmail.com>
2022-06-03 12:58:20 +0200

    Update Help.java to use updated builder interface

--- src/test/java/com/trustedservices/Help.java
+++ src/test/java/com/trustedservices/Help.java
@@ -3,24 +3,35 @@ package com.trustedservices;
 import com.trustedservices.domain.Country;
 import com.trustedservices.domain.TrustedList;
 import com.trustedservices.navigator.web.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Help {
     private static TrustedList wholeList;
 
     private static void constructWholeList() {
         try {
-            TrustedListJsonBuilder builder = new TrustedListJsonBuilder();
-
-            Path countries = Path.of("src/test/java/com/trustedservices/navigator/dummyCopyTrustedList/countryListDummy.json");
-            Path providers = Path.of("src/test/java/com/trustedservices/navigator/dummyCopyTrustedList/providerListDummy.json");
-            builder.setCountriesJson(Files.readString(countries));
-            builder.setProvidersJson(Files.readString(providers));
+            TrustedListJsonBuilder builder = setupFileBuilder();
 
             wholeList = builder.build();
         } catch (IOException e) {
             System.err.println("Error reading file\n" + e.getMessage());
             throw new RuntimeException(e);
         }
+    }
+
+    @NotNull
+    private static TrustedListJsonBuilder setupFileBuilder() throws IOException {
+        TrustedListJsonBuilder builder = new TrustedListJsonBuilder();
+
+        Path countries = Path.of("src/test/java/com/trustedservices/navigator/dummyCopyTrustedList/countryListDummy.json");
+        Path providers = Path.of("src/test/java/com/trustedservices/navigator/dummyCopyTrustedList/providerListDummy.json");
+        builder.setCountriesJson(Files.readString(countries));
+        builder.setProvidersJson(Files.readString(providers));
+        return builder;
     }
 
     public static TrustedList getWholeList() {
