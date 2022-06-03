1716e56e73002c4a4f7e2e2c42c38f8da0e64e55
Mazzarotto Filippo <filippomazzarotto@gmail.com>
2022-06-03 12:57:52 +0200

    Add JsonBuilder Tests

---
+++ src/test/java/com/trustedservices/navigator/web/TrustedListJsonBuilderTest.java
@@ -0,0 +1,59 @@
+package com.trustedservices.navigator.web;
+
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("A TrustedListJsonBuilder")
+class TrustedListJsonBuilderTest {
+    TrustedListJsonBuilder trustedJsonBuilder;
+
+    @Test
+    @DisplayName("is created")
+    void isInstantiatedWithNewClass() {
+        new TrustedListJsonBuilder();
+    }
+
+    @Nested
+    @DisplayName("when new")
+    class WhenNew {
+
+        @BeforeEach
+        void createAnEmptyJsonBuilder() {
+            trustedJsonBuilder = new TrustedListJsonBuilder();
+        }
+
+        @Test
+        @DisplayName("using an empty builder should throw IllegalStateException")
+        void useEmptyJsonBuilder() {
+            assertThrows(IllegalStateException.class, () -> trustedJsonBuilder.build());
+        }
+    }
+
+    @Nested
+    @DisplayName("when set up with good json data")
+    class WhenSetUp {
+
+        @BeforeEach
+        void createAndSetupJsonBuilder() throws IOException {
+            trustedJsonBuilder = new TrustedListJsonBuilder();
+
+            Path countries = Path.of("src/test/java/com/trustedservices/navigator/dummyCopyTrustedList/countryListDummy.json");
+            Path providers = Path.of("src/test/java/com/trustedservices/navigator/dummyCopyTrustedList/providerListDummy.json");
+            trustedJsonBuilder.setCountriesJson(Files.readString(countries));
+            trustedJsonBuilder.setProvidersJson(Files.readString(providers));
+        }
+
+        @Test
+        @DisplayName("building the trusted list should not throw any exception")
+        void buildWithGoodBuilder() {
+            assertDoesNotThrow(() -> trustedJsonBuilder.build());
+        }
+    }
+
+
+}
