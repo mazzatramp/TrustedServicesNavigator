fc741e78ca860618a51630d5e3c6a2faab09ad50
Riccardo1000 <riccardocontelav@gmail.com>
2022-06-07 13:32:37 +0200

    Clean TrustedListJsonBuilderTest. It is ready

--- src/test/java/com/trustedservices/navigator/web/TrustedListJsonBuilderTest.java
+++ src/test/java/com/trustedservices/navigator/web/TrustedListJsonBuilderTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,72 +65,43 @@ class TrustedListJsonBuilderTest {
             TrustedList trustedList = trustedJsonBuilder.build();
             assertNotNull(trustedList);
         }
+
         @Test
-        @DisplayName("the trusted list built should represent the values in the json")
+        @DisplayName("the trusted list built should be equal to the trusted list with the values from the JSON")
         void builtRightTrustedList() {
             TrustedList trustedList = trustedJsonBuilder.build();
             TreeSet<Country> listOfCountries = new TreeSet<>();
 
-            Country country1 = new Country("Austria","AT");
+            Country country1 = new Country("Austria", "AT");
             TreeSet<String> serviceTypesDatakom = new TreeSet<>();
             serviceTypesDatakom.add("QCertESig");
             TreeSet<String> serviceTypesA_sign_premium_CA = new TreeSet<>();
             serviceTypesA_sign_premium_CA.add("QCertESig");
             TreeSet<Service> servicesDatakom = new TreeSet<>();
-            Provider Datakom_Austria_GmbH = new Provider(country1,3,"Datakom Austria GmbH","VATAT-U44837307", serviceTypesDatakom,servicesDatakom);
-            Service A_sign_premium_CA = new Service(Datakom_Austria_GmbH,1,"a-sign Premium CA","http://uri.etsi.org/TrstSvc/Svctype/CA/QC","withdrawn",serviceTypesA_sign_premium_CA);
+            Provider Datakom_Austria_GmbH = new Provider(country1, 3, "Datakom Austria GmbH", "VATAT-U44837307", serviceTypesDatakom, servicesDatakom);
+            Service A_sign_premium_CA = new Service(Datakom_Austria_GmbH, 1, "a-sign Premium CA", "http://uri.etsi.org/TrstSvc/Svctype/CA/QC", "withdrawn", serviceTypesA_sign_premium_CA);
             servicesDatakom.add(A_sign_premium_CA);
             country1.getProviders().add(Datakom_Austria_GmbH);
 
-            Country country2 = new Country("Belgium","Be");
-            TreeSet <String> serviceTypesConnective = new TreeSet<>();
+            Country country2 = new Country("Belgium", "Be");
+            TreeSet<String> serviceTypesConnective = new TreeSet<>();
             serviceTypesConnective.add("QValQESig");
             serviceTypesConnective.add("QValQESeal");
-            TreeSet <String> serviceTypesConnective_Validation_Service = new TreeSet<>();
+            TreeSet<String> serviceTypesConnective_Validation_Service = new TreeSet<>();
             serviceTypesConnective_Validation_Service.add("QValQESig");
             serviceTypesConnective_Validation_Service.add("QValQESeal");
-            TreeSet <Service> servicesConnective = new TreeSet<>();
-            Provider Connective = new Provider(country2,13,"CONNECTIVE","VATBE-0467046486", serviceTypesConnective,servicesConnective);
-            Service Connective_Validation_Service = new Service(Connective,1,"Connective Validation Service","http://uri.etsi.org/TrstSvc/Svctype/QESValidation/Q","granted",serviceTypesConnective_Validation_Service);
+            TreeSet<Service> servicesConnective = new TreeSet<>();
+            Provider Connective = new Provider(country2, 13, "CONNECTIVE", "VATBE-0467046486", serviceTypesConnective, servicesConnective);
+            Service Connective_Validation_Service = new Service(Connective, 1, "Connective Validation Service", "http://uri.etsi.org/TrstSvc/Svctype/QESValidation/Q", "granted", serviceTypesConnective_Validation_Service);
             servicesConnective.add(Connective_Validation_Service);
             country2.getProviders().add(Connective);
-
             listOfCountries.add(country1);
             listOfCountries.add(country2);
             TrustedList testTrustedList = new TrustedList(listOfCountries);
             testTrustedList.updateServiceTypesAndStatuses();
 
-            System.out.println(testTrustedList.getCountries());
-            System.out.println(trustedList.getCountries());
-           // System.out.println(testTrustedList.getCountries().stream());
-            //System.out.println(trustedList.getCountries().stream().findAll().get().getProviders());
-            System.out.println(testTrustedList.getServiceTypes());
-            System.out.println(trustedList.getServiceTypes());
-            System.out.println(testTrustedList.getStatuses());
-            System.out.println(testTrustedList.getStatuses());
+            assertEquals(trustedList, testTrustedList);
 
-            Set<Country> countryExpected = new HashSet<>();
-            countryExpected.add(country1);
-            countryExpected.add(country2);
-            Set<Provider> providersExpected = new HashSet<>();
-            providersExpected.add(Datakom_Austria_GmbH);
-            providersExpected.add(Connective);
-            Set<Service> servicesExpected = servicesDatakom;
-            boolean areEqualList= trustedList.equals(testTrustedList);
-            System.out.println("liste ug "+ areEqualList);
-            assertEquals(trustedList,testTrustedList);
-            System.out.println(trustedList.equals(testTrustedList));
-            trustedList.getCountries().forEach(country -> {
-                System.out.println("country" + countryExpected.contains(country));
-                country.getProviders().forEach(provider -> {
-                    System.out.println("provider" + providersExpected.contains(provider));
-                    provider.getServices().forEach(service ->{
-                        servicesExpected.contains(service);
-                       // System.out.println("service types" + testTrustedList.getServiceTypes().contains(service.getServiceTypes()));
-                       // System.out.println(testTrustedList.getStatuses().contains(service.getStatus()));
-                    });
-                });
-            });
         }
     }
 
